a1c0b4488ba86d502182f81da9654dca119d2a0a
ABDULAZIZ ALRASHDI <bogico.sa@gmail.com>
2026-01-09 16:40:55 +0300

    Add namespaces for API endpoints
    Import and register namespaces for users, amenities, places, and reviews in the API.

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restx import Api
 
+
 def create_app():
     app = Flask(__name__)
     api = Api(
@@ -11,6 +12,15 @@ def create_app():
         doc="/api/v1/",
     )
 
-    # Namespaces will be added later (users/places/reviews/amenities)
+    # Import and register namespaces
+    from hbnb.app.api.v1.users import api as users_ns
+    from hbnb.app.api.v1.amenities import api as amenities_ns
+    from hbnb.app.api.v1.places import api as places_ns
+    from hbnb.app.api.v1.reviews import api as reviews_ns
+
+    api.add_namespace(users_ns, path='/api/v1/users')
+    api.add_namespace(amenities_ns, path='/api/v1/amenities')
+    api.add_namespace(places_ns, path='/api/v1/places')
+    api.add_namespace(reviews_ns, path='/api/v1/reviews')
 
     return app
